704d0690589848de175e8e8d7026be33ac8cf8f6
Yerlyn Montiel <108438134+YerlynSnowflake@users.noreply.github.com>
2022-06-29 17:53:23 -0600

    Update streamlit_app.py
    adding breakfast menu section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New HEalthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Breakfast Menu')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
